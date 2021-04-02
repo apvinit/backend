8085ba55b91ce78c3fbc2456a0208fbb1214733c
h4rdw1r3 <vinaugust6@gmail.com>
2021-04-02 18:15:44 +0530

    remove unnecessary config values

--- conf/conf.go
+++ conf/conf.go
@@ -1,14 +1,5 @@
 package conf
 
-// DatabaseIP is the IP Adrress for the database
-const DatabaseIP = "127.0.0.1"
-
-// DatabaseName is the name of database in use
-const DatabaseName = "test"
-
-// ServerPort for starting server on port
-const ServerPort = ":1323"
-
 // DynamicLinkAPIKey is key required for genrating dynamic links
 const DynamicLinkAPIKey = "AIzaSyD5nKQMrrZnRN089ynNPQ6z0AJPxe1j-hA"
 
@@ -32,11 +23,7 @@ const DeepLinkDomain = "https://jbda.in/"
 // DefaultImageLink is used when imageLink is not passed in the create post request (same for debug & prod)
 const DefaultImageLink = "https://firebasestorage.googleapis.com/v0/b/job-adda.appspot.com/o/images%2Fbanner.jpg?alt=media&token=2081e3ff-b79e-4cd9-9b6b-ae65a74336b8"
 
-// const DatabaseIP = "127.0.0.1"
-
-// const DatabaseName = "jobadda"
-
-// const ServerPort = ":8080"
+const ServerPort = ":8080"
 
 // const DynamicLinkAPIKey = "AIzaSyCAR4-s7t0iN8ZGQjUl__1Q0miAA3x7CuU"
 
